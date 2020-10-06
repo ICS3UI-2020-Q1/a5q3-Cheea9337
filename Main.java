6d602bb96215b3872f8a35af788f84b4068e92cd
Cheea9337 <cheea9337@wrdsb.ca>
2020-10-06 14:58:45 +0000

    finished a5q3

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for an integer
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int userNum = input.nextInt();
+
+    int sum = 1;
+    for(int count = userNum; count >= 1; count--){
+      sum = sum * count;
+    }
+    System.out.println(userNum + "!" + " = " + sum);
   }
 }
